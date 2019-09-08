b225ba358d10d8c2f3cb635ea7a5f115b73dd753
Jacksta <Jack.Dillon266@gmail.com>
2019-09-08 21:27:52 +1000

    implement map services

---
+++ pet-clinic-data/src/main/java/pets/com/au/sfgpetclinic/service/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package pets.com.au.sfgpetclinic.service.map;
+
+import pets.com.au.sfgpetclinic.model.Pet;
+import pets.com.au.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
